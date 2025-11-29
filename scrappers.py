adaf8ff3fab55c5f3a09afd79b7381edad8b6d9c
allter <adam.nowak789@onet.pl>
2025-11-29 17:33:50 +0100

    update: new details addded

--- scrappers.py
+++ scrappers.py
@@ -12,11 +12,26 @@ def olx_scrapper(soup):
         link_tag = card.find('a', href=lambda s: s and '/oferta/praca/' in s)
         link = link_tag['href'] if link_tag else None
 
+        detailed_data = card.select('.css-w0dc4x')
+        # print(detailed_data)
+
+        city_tag = detailed_data[0].select('.css-jw5wnz')[0].get_text()
+        city = city_tag if city_tag else None
+        
+        availability_tag = detailed_data[1].get_text()
+        availability = availability_tag if availability_tag else None
+
+        contract_type_tag = detailed_data[2].get_text()
+        contract_type = contract_type_tag if contract_type_tag else None
+
         offers.append({
             "id": i,
             "name": name,
             "salary": salary,
-            "link": "https://olx.pl" + link
+            "link": "https://olx.pl" + link,
+            "city": city,
+            "availability": availability,
+            "contract_type": contract_type,
         })
 
     return {"title": "OLX oferty pracy", "offers": offers}
